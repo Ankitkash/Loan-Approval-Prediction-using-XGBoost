794dc14db1fb72be472b6eaad5937c3e2ef477b6
Ankitkash <49473464+Ankitkash@users.noreply.github.com>
2026-04-24 23:39:53 +0530

    Update Loan_Approval_app.py

--- Loan_Approval_app.py
+++ Loan_Approval_app.py
@@ -12,7 +12,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+#import joblib
 import os
 
 # ──────────────────────────────────────────────────────────────────────────
